810d6bb736d78d4dfdbce74ef7bfe2a30a799a76
木子 <lqclh502@sina.com>
2019-08-06 17:02:44 +0800

    删除无效代码

--- src/main/java/com/lqc/utils/SecurityUtil.java
+++ src/main/java/com/lqc/utils/SecurityUtil.java
@@ -8,7 +8,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.math.BigDecimal;
 import java.net.URLEncoder;
 import java.security.GeneralSecurityException;
 import java.security.MessageDigest;
